1c7f8e728a32f4e061d74f6ae9cd765f1d3c0b1c
irakli-dadu <lipsowow@gmail.com>
2022-08-10 01:06:54 +0400

    some logics done some left

---
+++ script.js
@@ -0,0 +1,72 @@
+let hamburger = document.querySelector(".hamburger")
+let navMenu = document.querySelector(".nav-menu")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+
+})
+
+document.querySelectorAll(".nav-link").forEach(n => 
+    n.addEventListener("click", () => {
+        hamburger.classList.remove("active")
+        navMenu.classList.remove("active")
+
+}))
+
+// -------------
+
+
+let lineInput = document.querySelector('.inputClass')
+
+let buttonAdd = document.querySelector('.button')
+
+let inputUl = document.querySelector('.ghost-ul')
+
+let formBox = document.querySelector('.form-box')
+
+let clearBtn = document.createElement('button')
+     clearBtn.textContent = 'Clear All'
+     clearBtn.style.color = 'crimson'
+     clearBtn.addEventListener('click', function() {
+        inputUl.innerHTML = ' '
+     })
+
+     formBox.appendChild(clearBtn)
+
+
+buttonAdd.addEventListener('click', function() {
+    let response = lineInput.value
+
+    if (response == ' ') {
+        alert('type')
+        return
+    }
+
+     let newLi = document.createElement('li')
+     let delBtn = document.createElement('button')
+     delBtn.textContent = 'delete'
+     delBtn.addEventListener('click', function() {
+        newLi.remove()
+     })
+    newLi.textContent = response
+
+    inputUl.appendChild(newLi)
+
+    newLi.appendChild(delBtn)
+
+    lineInput.value = ' '
+})
+
+
+
+
+
+
+
+
+
+
+lineInput.addEventListener('focus', function(objectEvent) {
+    objectEvent.target.style.border = '2px solid crimson'
+})
